4c4c2ea2d17d8937975902db3de10e9aaedb0c93
Daniel Mason <daniel.mason@apolitical.co>
2019-06-20 11:19:40 +0100

    Added: Test against HashMaps

--- kv-predicates/src/lib.rs
+++ kv-predicates/src/lib.rs
@@ -77,4 +77,33 @@ mod tests {
             Some(("three", "3".to_string()))
         );
     }
+
+    #[test]
+    fn filter_map_predicate_with_hash_str_string() {
+        use std::collections::HashMap;
+
+        let test_data: HashMap<String, String> = vec![
+            ("one".to_string(), "1".to_string()),
+            ("two".to_string(), "2".to_string()),
+            ("three".to_string(), "3".to_string()),
+        ]
+        .into_iter()
+        .collect();
+
+        let mut tester = PredicateTester::default();
+
+        let remaining_data: Vec<(String, String)> = test_data
+            .into_iter()
+            .filter_map(|v| tester.filter_map_predicate(v))
+            .collect();
+
+        assert_eq!(tester.one, "1".to_string());
+        assert_eq!(tester.two, "2".to_string());
+
+        assert_eq!(remaining_data.len(), 1);
+        assert_eq!(
+            remaining_data.into_iter().next(),
+            Some(("three".to_string(), "3".to_string()))
+        );
+    }
 }
