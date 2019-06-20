353701e12209dc64f0a98d3cfbdad2d72761d429
Daniel Mason <daniel.mason@apolitical.co>
2019-06-20 11:10:45 +0100

    Added: Basic trait

--- kv-predicates/src/lib.rs
+++ kv-predicates/src/lib.rs
@@ -1,7 +1,53 @@
+pub trait KVPredicates {
+    fn filter_map_predicate(&mut self, current: (String, String)) -> Option<(String, String)>;
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::KVPredicates;
+
+    #[derive(Default)]
+    struct PredicateTester {
+        pub one: String,
+        pub two: String,
+    }
+
+    impl KVPredicates for PredicateTester {
+        fn filter_map_predicate(
+            &mut self,
+            (key, value): (String, String),
+        ) -> Option<(String, String)> {
+            match key.as_ref() {
+                "one" => self.one = value,
+                "two" => self.two = value,
+                _ => return Some((key, value)),
+            }
+            None
+        }
+    }
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let test_data = vec![
+            ("one".to_string(), "1".to_string()),
+            ("two".to_string(), "2".to_string()),
+            ("three".to_string(), "3".to_string()),
+        ];
+
+        let mut tester = PredicateTester::default();
+
+        let remaining_data: Vec<(String, String)> = test_data
+            .into_iter()
+            .filter_map(|v| tester.filter_map_predicate(v))
+            .collect();
+
+        assert_eq!(tester.one, "1".to_string());
+        assert_eq!(tester.two, "2".to_string());
+
+        assert_eq!(remaining_data.len(), 1);
+        assert_eq!(
+            remaining_data.into_iter().next(),
+            Some(("three".to_string(), "3".to_string()))
+        );
     }
 }
