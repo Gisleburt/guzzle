522a04909ac297498dc0001327db80684ca1cd8a
Daniel Mason <daniel.mason@apolitical.co>
2019-07-04 14:18:53 +0100

    Changed: Extracted derive for structs with named fields

--- yummy-metadata-derive/src/lib.rs
+++ yummy-metadata-derive/src/lib.rs
@@ -1,14 +1,15 @@
 extern crate proc_macro;
 
 use crate::proc_macro::TokenStream;
-use quote::quote;
-use syn::{Data, Field, Fields, Ident, Type};
+use quote::{quote, quote_spanned};
+use syn::spanned::Spanned;
+use syn::{parse_macro_input, Data, DeriveInput, Field, Fields, FieldsNamed, Ident, Type};
 
 #[proc_macro_derive(YummyMetadata)]
 pub fn yummy_metadata_macro_derive(input: TokenStream) -> TokenStream {
     // Construct a representation of Rust code as a syntax tree
     // that we can manipulate
-    let ast = syn::parse(input).unwrap();
+    let ast = parse_macro_input!(input);
 
     // Build the trait implementation
     impl_yummy_metadata(&ast)
@@ -22,36 +23,40 @@ fn field_to_type_and_ident(field: &Field) -> Option<(&Type, &Ident)> {
     }
 }
 
-fn impl_yummy_metadata(ast: &syn::DeriveInput) -> TokenStream {
-    if let Data::Struct(s) = &ast.data {
-        if let Fields::Named(fields) = &s.fields {
-            let (_types, idents): (Vec<&Type>, Vec<&Ident>) = fields
-                .named
-                .iter()
-                .filter_map(field_to_type_and_ident)
-                .unzip();
-            let (keys, idents): (Vec<String>, Vec<&Ident>) =
-                idents.into_iter().map(|i| (format!("{}", i), i)).unzip();
-
-            let name = &ast.ident;
-            let gen = quote! {
-                impl YummyMetadata for #name {
-                    fn eat_yummy_metadata<T>(&mut self, (key, value): (T, String)) -> Option<(T, String)>
-                    where T: AsRef<str>
-                    {
-                        match key.as_ref() {
-                            #( #keys => self.#idents = value, )*
-                            _ => return Some((key, value)),
-                        };
-                        None
-                    }
-                }
-            };
-            gen.into()
-        } else {
-            panic!("yummy_metadata currently only work for structs")
-        }
-    } else {
-        panic!("yummy_metadata currently only work for structs")
+fn impl_yummy_metadata(ast: &DeriveInput) -> TokenStream {
+    match &ast.data {
+        Data::Struct(s) => match &s.fields {
+            Fields::Named(fields) => impl_yummy_metadata_named_fields(ast, fields),
+            _ => unimplemented!(),
+        },
+        _ => unimplemented!(),
     }
+}
+
+fn impl_yummy_metadata_named_fields(ast: &DeriveInput, fields: &FieldsNamed) -> TokenStream {
+    let name = &ast.ident;
+    let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+
+    let (matchers, keys): (Vec<String>, Vec<&Ident>) = fields
+        .named
+        .iter()
+        .map(|field| field.ident.as_ref())
+        .filter_map(|ident| ident)
+        .map(|ident| (ident.to_string(), ident))
+        .unzip();
+
+    let gen = quote! {
+        impl #impl_generics YummyMetadata for #name #ty_generics #where_clause {
+            fn eat_yummy_metadata<T>(&mut self, (key, value): (T, String)) -> Option<(T, String)>
+            where T: AsRef<str>
+            {
+                match key.as_ref() {
+                    #( #matchers => self.#keys = value, )*
+                    _ => return Some((key, value)),
+                };
+                None
+            }
+        }
+    };
+    gen.into()
 }
